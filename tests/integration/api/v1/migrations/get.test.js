0191dd57f2e2b41983cac704366e6f109d595011
João Leite <62863517+j-cleite@users.noreply.github.com>
2025-09-26 15:17:28 +0000

    adds `api/v1/migrations` endpoint

---
+++ tests/integration/api/v1/migrations/get.test.js
@@ -0,0 +1,17 @@
+import database from "infra/database.js";
+
+beforeAll(cleanDatabase);
+
+async function cleanDatabase() {
+  await database.query("DROP SCHEMA public cascade; CREATE SCHEMA public;");
+}
+
+test("GET to /api/v1/migrations should return 200", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/migrations");
+  expect(response.status).toBe(200);
+
+  const resposeBody = await response.json();
+  console.log(resposeBody);
+  expect(Array.isArray(resposeBody)).toBe(true);
+  expect(resposeBody.length).toBeGreaterThan(0);
+});
---
+++ tests/integration/api/v1/migrations/post.test.js
@@ -0,0 +1,27 @@
+import database from "infra/database.js";
+
+beforeAll(cleanDatabase);
+
+async function cleanDatabase() {
+  await database.query("DROP SCHEMA public cascade; CREATE SCHEMA public;");
+}
+
+test("POST to /api/v1/migrations should return 200", async () => {
+  const response1 = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "POST",
+  });
+  expect(response1.status).toBe(201);
+  const respose1Body = await response1.json();
+  console.log(respose1Body);
+  expect(Array.isArray(respose1Body)).toBe(true);
+  expect(respose1Body.length).toBeGreaterThan(0);
+
+  const response2 = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "POST",
+  });
+  expect(response2.status).toBe(200);
+  const respose2Body = await response2.json();
+  console.log(respose2Body);
+  expect(Array.isArray(respose2Body)).toBe(true);
+  expect(respose2Body.length).toBe(0);
+});
